6c898e05b86bc17c1d354568f07f2917bc8beea7
arnobtarafder <rmtarnob@gmail.com>
2022-05-12 21:46:30 +0600

    add: navigation bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Pages/About';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      
+      <Navbar>
+   
+      </Navbar>
+
 
     </div>
   );
