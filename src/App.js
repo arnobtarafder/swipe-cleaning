4c1697b5962439b7a478eb7a3c5213e84b0c6958
arnobtarafder <rmtarnob@gmail.com>
2022-05-13 17:01:33 +0600

    fix: tailwind config file structure & color combinations with mood

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import About from './Pages/About';
+import Contact from './Pages/Contact';
 import Home from './Pages/Home';
+import Services from './Pages/Services';
+// import Login from './Pages/Login';
 
 function App() {
   return (
     <div>
-      
+
       <Navbar>
-   
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+          {/* <Route path='/login' element={<Login />} /> */}
+        </Routes>
       </Navbar>
 
 
