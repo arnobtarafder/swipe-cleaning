25137e8c48a3d582648d95f0e9ac362c4fdb53cc
arnobtarafder <rmtarnob@gmail.com>
2022-05-13 20:58:56 +0600

    fix: estimate form location

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Landing = () => {
     return (
-        <>
+        <div className="relative">
         <div class="hero h-screen lg:h-[60vh] bg-accent mt-16 lg:px-36">
             <div class="hero-content flex-col lg:flex-row md:flex-row">
                 <div className="">
@@ -17,36 +17,60 @@ const Landing = () => {
             </div>  
         </div>
 
-        <div className="">
+        <div className="relative rounded-2xl mx-auto mt-[-50px] bg-base-100 z-20 shadow-lg p-10 w-5/6">
                 <h1>Get Free Estimate</h1>
-                <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 <input 
                 type="text" 
                 placeholder="Type here" 
-                class="input input-bordered w-full max-w-xs" 
+                class="input input-bordered w-full" 
                 />
                 
                 <input 
                 type="text" 
                 placeholder="Type here" 
-                class="input input-bordered w-full max-w-xs" 
+                class="input input-bordered w-full" 
                 />
                 
                 <input 
                 type="text" 
                 placeholder="Type here" 
-                class="input input-bordered w-full max-w-xs" 
+                class="input input-bordered w-full" 
                 />
                 
                 <input 
                 type="text" 
                 placeholder="Type here" 
-                class="input input-bordered w-full max-w-xs" 
+                class="input input-bordered w-full" 
+                /> 
+                
+                 <input 
+                type="text" 
+                placeholder="Type here" 
+                class="input input-bordered w-full" 
+                />
+                
+                <input 
+                type="text" 
+                placeholder="Type here" 
+                class="input input-bordered w-full" 
+                />
+                
+                <input 
+                type="text" 
+                placeholder="Type here" 
+                class="input input-bordered w-full" 
+                />
+                
+                <input 
+                type="text" 
+                placeholder="Type here" 
+                class="input input-bordered w-full" 
                 />
 
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
