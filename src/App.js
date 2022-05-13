8fe6b07818b09c9affb0177da7622cc01a312b76
arnobtarafder <rmtarnob@gmail.com>
2022-05-13 18:18:29 +0600

    add: public route component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Home from './Pages/Home';
-import Services from './Pages/Services';
-import Login from './Pages/Login';
+import {publicRoute} from "./Routes/publicRoutes"
 
 
-const publicRoute = [
-  { path: "/", name: "Home", Component: Home },
-  { path: "/about", name: "About", Component: About },
-  { path: "/contact", name: "Contact", Component: Contact },
-  { path: "/services", name: "Services", Component: Services },
-  { path: "/login", name: "Login", Component: Login },
-]
+
 
 
 function App() {
