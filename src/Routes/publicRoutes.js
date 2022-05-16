7fa93cf9f8e76f7b94e2e2d7384564b5638cf09d
arnobtarafder <rmtarnob@gmail.com>
2022-05-16 21:10:48 +0600

    add: private routes

--- src/Routes/publicRoutes.js
+++ src/Routes/publicRoutes.js
@@ -6,8 +6,6 @@ import Login from '../Pages/Login';
 
 export const publicRoute = [
     { path: "/", name: "Home", Component: Home },
-    { path: "/about", name: "About", Component: About },
     { path: "/contact", name: "Contact", Component: Contact },
-    { path: "/services", name: "Services", Component: Services },
     { path: "/login", name: "Login", Component: Login },
   ]
