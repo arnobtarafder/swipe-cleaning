a9f75492ee075f591107c455b9e5c3b2fd303f39
arnobtarafder <rmtarnob@gmail.com>
2022-05-16 21:38:51 +0600

    fix: home route routing

--- src/Routes/publicRoutes.js
+++ src/Routes/publicRoutes.js
@@ -4,6 +4,7 @@ import Login from '../Pages/Login';
 
 export const publicRoutes = [
     { path: "/", name: "Home", Component: Home },
+    { path: "/home", name: "Home", Component: Home },
     { path: "/contact", name: "Contact", Component: Contact },
     { path: "/login", name: "Login", Component: Login },
   ]
